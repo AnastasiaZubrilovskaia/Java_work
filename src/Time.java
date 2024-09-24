ea07c796f388629aa9939dc7a6ea06b3e8ea5d3f
Anastasia <zubrilovsckaya@yandex.ru>
2024-09-24 11:47:39 +0400

    Home_work 24.09 (part 3)

--- src/Time.java
+++ src/Time.java
@@ -7,9 +7,17 @@ public class Time {
         this.seconds = seconds%(3600*24);
     }
     public String toString(){
+        String res = "";
         hours = seconds / 3600;
+        if (hours/10 == 0) res = res + "0" + hours + ":";
+        else res = res +  hours + ":";
         minutes = seconds % 3600 / 60;
+        if (minutes/10 == 0) res = res + "0" +  minutes + ":";
+        else res = res +  minutes + ":";
         second = seconds % 60;
-        return hours + ":" + minutes + ":" + second;
+        if (second/10 == 0) res = res + "0" + second;
+        else res = res +  second;
+
+        return res;
     }
 }
